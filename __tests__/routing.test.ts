770144542ef1bf1dc508a534bf02f065e1edf993
Marcin Chudy <marcinchudy94@gmail.com>
2020-09-14 17:01:21 +0200

    Configure tests

---
+++ __tests__/routing.test.ts
@@ -0,0 +1,20 @@
+import { createRouting, segment, number } from 'index';
+
+describe('routing', () => {
+  it('should create routing', () => {
+    const routes = createRouting({
+      company: segment`/companies/${number('companyId')}`,
+      contact: segment`/contacts/${number('contactId')}`,
+      insight: segment`/insight/${number('insightId')}`,
+      admin: {
+        ...segment`/admin`,
+        children: {
+          users: segment`/users`,
+        },
+      },
+      onetime: segment`/onetime`,
+    } as const);
+
+    routes.company({ companyId: '20' });
+  });
+});
--- src/RouteDescription.ts
+++ src/RouteDescription.ts
@@ -1,29 +1,45 @@
-import PathPattern, { PathOptionalParams, PathOptionalQuery, PathRequiredParams, PathRequiredQuery } from "PathPattern";
+import PathPattern, {
+  PathOptionalParams,
+  PathOptionalQuery,
+  PathRequiredParams,
+  PathRequiredQuery,
+} from 'PathPattern';
 
 export default interface RouteDescription<
-    TRequiredParams extends string = string,
-    TOptionalParams extends string = string,
-    TRequiredQuery extends string = string,
-    TOptionalQuery extends string = string,
-    TChildren extends { readonly [name: string]: RouteDescription } = any
+  TRequiredParams extends string = string,
+  TOptionalParams extends string = string,
+  TRequiredQuery extends string = string,
+  TOptionalQuery extends string = string,
+  TChildren extends { readonly [name: string]: RouteDescription } = any
 > {
-    readonly pattern: PathPattern<TRequiredParams, TOptionalParams, TRequiredQuery, TOptionalQuery>;
-    readonly children?: TChildren;
+  readonly pattern: PathPattern<
+    TRequiredParams,
+    TOptionalParams,
+    TRequiredQuery,
+    TOptionalQuery
+  >;
+  readonly children?: TChildren;
 }
 
-export type PathPatternOf<TRouteDescription extends RouteDescription> = TRouteDescription["pattern"];
-    
-export type RouteRequiredParams<TRouteDescription extends RouteDescription> = PathRequiredParams<PathPatternOf<TRouteDescription>>;
-export type RouteOptionalParams<TRouteDescription extends RouteDescription> = PathOptionalParams<PathPatternOf<TRouteDescription>>;
-export type RouteRequiredQuery<TRouteDescription extends RouteDescription> = PathRequiredQuery<PathPatternOf<TRouteDescription>>;
-export type RouteOptionalQuery<TRouteDescription extends RouteDescription> = PathOptionalQuery<PathPatternOf<TRouteDescription>>;
+export type PathPatternOf<
+  TRouteDescription extends RouteDescription
+> = TRouteDescription['pattern'];
 
-export type RouteChildren<TRD extends RouteDescription> = TRD extends RouteDescription<
-    string,
-    string,
-    string,
-    string,
-    infer C
->
-    ? C
-    : never;
+export type RouteRequiredParams<
+  TRouteDescription extends RouteDescription
+> = PathRequiredParams<PathPatternOf<TRouteDescription>>;
+export type RouteOptionalParams<
+  TRouteDescription extends RouteDescription
+> = PathOptionalParams<PathPatternOf<TRouteDescription>>;
+export type RouteRequiredQuery<
+  TRouteDescription extends RouteDescription
+> = PathRequiredQuery<PathPatternOf<TRouteDescription>>;
+export type RouteOptionalQuery<
+  TRouteDescription extends RouteDescription
+> = PathOptionalQuery<PathPatternOf<TRouteDescription>>;
+
+export type RouteChildren<
+  TRD extends RouteDescription
+> = TRD extends RouteDescription<string, string, string, string, infer C>
+  ? C
+  : never;
