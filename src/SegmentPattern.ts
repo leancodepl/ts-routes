ce34d48f287adefd3388e27312ceb51ba15a6b46
Ignacy Ruszpel <8687205+iruszpel@users.noreply.github.com>
2023-09-07 12:48:33 +0200

    chore: cleanup workspace

--- src/SegmentPattern.ts
+++
@@ -1,6 +0,0 @@
-import { Optionality } from "./helpers";
-import PathParamDescription from "./PathParamDescription";
-
-export default class SegmentPattern<TPathParamsDescription extends PathParamDescription<string, Optionality>[]> {
-    constructor(public readonly pattern: string, public readonly params: TPathParamsDescription) {}
-}
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as createRouting } from "./createRouting";
-export { default as RouteDescription } from "./RouteDescription";
-export { default as segment } from "./segment";
-export { default as string } from "./parameters/string";
-export { default as number } from "./parameters/number";
-export { default as uuid } from "./parameters/uuid";
-export { default as arg } from "./parameters/arg";
-export { default as query } from "./query";
-export { PathParamsFor, QueryParamsFor } from "./helpers";
